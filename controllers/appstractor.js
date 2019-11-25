209f34e813145478c5fff659593db50c22806d23
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2019-11-25 01:17:52 -0500

    test

--- controllers/appstractor.js
+++ controllers/appstractor.js
@@ -56,6 +56,7 @@ const currentUser = {username: 'Username', email: 'first.last@gmail.com', passwo
     router.post('/saved/:id', (req, res) => {
             //capture chrome makes a get request to /saved/:id
             capture({
+                arg: '--disable-dev-shm-usage', 
                 url: `https://appstractor.herokuapp.com/appstractor/saved/${req.params.id}`,
                 width: 3600,
                 height: 2400,

27dcf130c4ea45ee963fbb8d00d64f24a8e31c98
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2019-11-26 18:25:06 -0500

    test

--- models/appstraction.js
+++ models/appstraction.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const appstraction = new Schema({
     user: {type: String, required: true},
     dom: {type: String, required: true}
-});
+}, {timestamps: true});
 
 const Appstraction = mongoose.model('Appstractions', appstraction);
 module.exports = Appstraction; 

fee6fe02f05fa37d9afd590c52b98cf5731ea865
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2019-11-27 18:14:24 -0500

    test

--- db/heroku.js
+++
@@ -1,8 +0,0 @@
-// for heroku connection
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb://pavlos:B1u3m00n@ds249008.mlab.com:49008/heroku_0thh8tzc';
-
-mongoose.connect(mongoURI);
-mongoose.Promise = Promise;
-
-module.exports = mongoose;
